a4c0c9c3c3dfd215df05b65b074541905208de3a
Aidan Olsen <a.olsen2024@icloud.com>
2020-08-01 14:33:16 -0600

    Perlin Noise

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include "image.h"
+#include "common/perlin_noise.h"
 
 // TODO: sort layers by z_lvl
 
-int main(int, char**) {
+int main(int argc, char** argv) {
 	// init vars
 	unsigned int width = 255, height = 255;
 	layer* base = new layer(width, height, 0);
 	image img(width, height);
+
+	int _seed = 16783;
 
 //	img.add_layer(base);
 
@@ -17,9 +20,20 @@ int main(int, char**) {
 		for (int y = 1; y <= height; y++)
 		{
 			// nice colorful gradient
-			int r = height-y + 1;
-			int b = width-x + 1;
-			int g = 255;
+			//int r = height-y + 1;
+			//int b = width-x + 1;
+			//int g = 255;
+
+			// perlin noise
+			perlin_noise np(_seed);
+			float v = np.noise(x*0.000001, y*0.01, 0) * 255;
+			
+			int n = 30;
+			if (x <= v) n = 235;
+
+			int r = n;
+			int g = n;
+			int b = n;
 
 			base->set_pixel(y, x, new pixel(r, g, b));
 		}
